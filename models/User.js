1e33292453469d72043bcd4eb783f446e2bf5b92
DESKTOP-5PQ3RHD\lee hye young <db1magic@naver.com>
2020-11-02 17:40:19 +0900

    1102 비밀번호 암호화(bcrypt)

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+//salt가 몇글자인지 설정
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -26,6 +28,25 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save',function( next ){
+    var user = this;
+
+    if(user.isModified('password')) {
+//password를 변경할때만 실행되도록 조건문
+//저장하기 전에 function을 한다.
+//비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            //스키마의 user.password를 가져오고, salt, 암호화된 비밀번호(hash)를 완성한다.                       
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if(err) return next(err)
+                user.password = hash //들어있던 비밀번호를 hash된 비밀번호로 바꾼다.
+                next()
+            });
+        });
+    }
+});
 
 const User = mongoose.model('User',userSchema)
 
